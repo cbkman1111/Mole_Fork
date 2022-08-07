f3f165bda7f35b13340246eed9c11ac741c3cd3d
bkchoi79 <bkchoi79@gmail.com>
2022-08-07 21:21:57 +0900

    케릭터 및 몬스터 추가.
    기존 데이터 적용.

--- Assets/Scripts/Scenes/SceneGame.cs
+++ Assets/Scripts/Scenes/SceneGame.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
+using static UnitBase;
 
 public class SceneGame : SceneBase
 {
+    private Player player = null;
+    private List<UnitBase> monsters = null;
+
     public SceneGame(SCENES scene) : base(scene)
     {
     }
@@ -16,6 +21,42 @@ public class SceneGame : SceneBase
             menu.InitMenu();
         }
 
+        Player prefabPlayer = ResourcesManager.Instance.LoadBundle<Player>("Player.prefab");
+        Pigeon prefabPigeon = ResourcesManager.Instance.LoadBundle<Pigeon>("Pigeon.prefab");
+        PigeonQueen prefabPigeonQueen = ResourcesManager.Instance.LoadBundle<PigeonQueen>("PigeonQueen.prefab");
+
+        player = Instantiate<Player>(prefabPlayer);
+        player.transform.position = Vector3.zero;
+
+        monsters = new List<UnitBase>();
+        for ( int i = 0; i < 20; i++)
+        {
+            UnitBase monster = null;
+            if(i == 0)
+            {
+                monster = Instantiate<PigeonQueen>(prefabPigeonQueen);
+            }
+            else
+            {
+                monster = Instantiate<Pigeon>(prefabPigeon);
+            }
+
+            int x = UnityEngine.Random.Range(0, 10) - 5;
+            int z = UnityEngine.Random.Range(0, 10) - 5;
+            int y = 0;
+            
+            monster.transform.position = new Vector3(x, y, z);
+
+            SkellAnimationState[] animations = { SkellAnimationState.idle, SkellAnimationState.run, SkellAnimationState.die, SkellAnimationState.attack};
+            int randIndex = UnityEngine.Random.Range(0, animations.Length);
+            string name = animations[randIndex].ToString();
+            monster.skel.state.SetAnimation(0, name, true);
+            monsters.Add(monster);
+        }
+
+
+        //player = Instantiate<PigeonQueen>(prefabPigeonQueen);
+        //player.transform.position = Vector3.zero;
         return true;
     }
 
