db33daa4d54f5f26795dd8637680d3519850dfd1
byoung ki choi <bkchoi79@gmail.com>
2023-06-05 01:39:01 +0900

    [데브 머지]

---
+++ Assets/Scripts/Scenes/SceneMatch3.cs
@@ -0,0 +1,56 @@
+using SweetSugar.Scripts.Core;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SceneMatch3 : SceneBase
+{
+    public SceneMatch3(SCENES scene) : base(scene)
+    {
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public override bool Init(JSONObject param)
+    {
+        /*
+        UIMenuMatch3 menu = UIManager.Instance.OpenMenu<UIMenuMatch3>("UIMenuMatch3");
+        if (menu != null)
+        {
+            menu.InitMenu();
+            menu.OnStartGame = (int level) => {
+                //LevelManager.THIS.gameStatus = GameState.PrepareGame;
+                //GUIUtils.THIS.StartGame();
+            };
+        }
+        */
+
+        return true;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Update()
+    {
+
+    }
+
+    public override void OnTouchBean(Vector3 position)
+    {
+
+    }
+
+    public override void OnTouchEnd(Vector3 position)
+    {
+       
+    }
+
+    public override void OnTouchMove(Vector3 position)
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/Menu/UIMenuMatch3.cs
@@ -0,0 +1,51 @@
+using SweetSugar.Scripts.Core;
+using SweetSugar.Scripts.GUI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMenuMatch3 : MenuBase
+{
+    int level = 1;
+
+    public Action<int> OnStartGame { get; set;}
+
+    public override void OnInit()
+    {
+
+    }
+
+    public bool InitMenu()
+    {
+
+        return true;
+    }
+
+    public override void OnValueChanged(InputField input, string str) 
+    {
+        string name = input.name;
+        if(name == "InputField - Level")
+        {
+            level = int.Parse(str);
+        }
+    }
+
+    protected override void OnClick(Button btn)
+    {
+        string name = btn.name;
+        if (name == "Button - Back")
+        {
+            AppManager.Instance.ChangeScene(SceneBase.SCENES.Menu);
+        }
+        else if (name == "Button - StartGame")
+        {
+            if (OnStartGame != null)
+                OnStartGame(level);
+
+
+        }
+    }
+}
